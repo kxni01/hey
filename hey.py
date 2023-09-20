802f6df26d874fb6c08c93329a927f591fd9b6a1
kxni01 <kanishka.b@intel.com>
2023-09-20 14:12:18 +0530

    adding code

--- hey.py
+++ hey.py
@@ -5,7 +5,6 @@ csvfile = pd.read_csv('dummy.csv')
 data = {}
 for key in list(csvfile.columns):
     data[key] = list(csvfile[key])
-#print(data)
 
 with open('data.json', 'a') as f:
     f.write(json.dumps(data, ensure_ascii=False, indent=4))
